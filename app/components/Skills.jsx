c25f6eb666fe6c749b00feaec0c1cd3ae735eaa1
Abdulrahman Ghubun <4010675@upm.edu.sa>
2024-02-14 23:08:00 +0300

    upload

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React, { useState } from "react";
-import { Accordion, 
-    AccordionSummary, 
-    AccordionDetails, 
-    Typography, 
-    ThemeProvider, 
-    createTheme } from "@mui/material";
+import {
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    Typography,
+    ThemeProvider,
+    createTheme
+} from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { motion } from "framer-motion";
 
@@ -48,55 +50,57 @@ const Skills = () => {
 
     const darkTheme = createTheme({
         palette: {
-            mode: 'dark', 
+            mode: 'dark',
         },
     });
 
     return (
         <section id="skills" className='pt-20'>
             <ThemeProvider theme={darkTheme}>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.75, ease: "easeOut" }}
-            >
-                <div className="conText flex flex-col justify-center items-center">
-                    <h1 className='text-center text-3xl lg:text-4xl font-bold text-white mt-4 pb-6 mb-8 md:mb-6 md:mt-10'>
-                        Skills
-                    </h1>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.75, ease: "easeOut" }}
+                >
+                    <div className="conText flex flex-col justify-center items-center">
+                        <h1 className='text-center text-3xl lg:text-4xl font-bold text-white mt-4 pb-6 mb-8 md:mb-6 md:mt-10'>
+                            Skills
+                        </h1>
 
-                    {skillsList.map((skill) => (
-                        <Accordion
-                            expanded={expanded === skill.id}
-                            onChange={handleExpansion(skill.id)}
-                            sx={{
-                                '& .MuiAccordionDetails-root': { display: expanded === skill.id ? 'block' : 'none' },
-                            }}
-                            key={skill.id}
-                            className=" w-full"
-                        >
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${skill.id}-content`} id={`${skill.id}-header`}>
-                                <Typography className="text-left text-xl lg:text-2xl font-samibold">{skill.title}</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                {skill.content.map((item, index) => (
-                                    <ul className="ps-8">
-                                        <li
-                                            key={index}
-                                            className=" list-disc text-lg lg:text-xl leading-relaxed">
-                                            {item}
-                                        </li>
-                                    </ul>
-                                ))}
-                            </AccordionDetails>
-                        </Accordion>
-                    ))}
-                </div>
-            </motion.div>
-        </ThemeProvider>
+                        {skillsList.map((skill) => (
+                            <Accordion
+                                expanded={expanded === skill.id}
+                                onChange={handleExpansion(skill.id)}
+                                sx={{
+                                    '& .MuiAccordionDetails-root': { display: expanded === skill.id ? 'block' : 'none' },
+                                }}
+                                key={skill.id}
+                                className=" w-full"
+                            >
+                                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${skill.id}-content`} id={`${skill.id}-header`}>
+                                    <Typography className="text-left text-xl lg:text-2xl font-samibold">
+                                        {skill.title}
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {skill.content.map((item, index) => (
+                                        <ul className="ps-8">
+                                            <li
+                                                key={index}
+                                                className=" list-disc text-lg lg:text-xl leading-relaxed">
+                                                {item}
+                                            </li>
+                                        </ul>
+                                    ))}
+                                </AccordionDetails>
+                            </Accordion>
+                        ))}
+                    </div>
+                </motion.div>
+            </ThemeProvider>
         </section>
-        
+
     );
 };
 
